1931dd95d98d31406c4649be98b8e930737b51a3
SimonAllenOliver <simonallenoliver@gmail.com>
2024-05-23 22:40:47 -0500

    start mobile optimization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,117 +7,124 @@ function App() {
   return (
     <>
 
-      <div className="firstSlide">
 
+      <div className='giantContainer'>
 
-        <div className='sectionZero'>
-          <div className='container'>
-            <div className='d-flex justify-content-end myIcons'>
-              <a href="https://github.com/simonallenoliver" className='headLinks' target="_blank"><i class="fa-brands fa-github"></i></a>
-              <a href="mailto:simonallenoliver@gmail.com" className='headLinks'><i class="fa-solid fa-envelope"></i></a>
-              <a href="http://www.linkedin.com/in/simon-oliver-66807213b" className='headLinks' target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
+        <div className="slideOne">
+          <div className='sectionZero'>
+            <div className='container'>
+              <div className='d-flex justify-content-end myIcons'>
+                <a href="https://github.com/simonallenoliver" className='headLinks' target="_blank"><i class="fa-brands fa-github"></i></a>
+                <a href="mailto:simonallenoliver@gmail.com" className='headLinks'><i class="fa-solid fa-envelope"></i></a>
+                <a href="http://www.linkedin.com/in/simon-oliver-66807213b" className='headLinks' target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
+              </div>
             </div>
           </div>
+
+          <div className='sectionOne d-flex align-items-center'>
+            <div className='container'>
+              <div className='row'>
+                <div className='col-sm-9 col-lg-7 mx-auto text-center'>
+                  <h1 id='simonOliver'>SIMON OLIVER</h1>
+                </div>
+              </div>
+              <div className='row pb-5'>
+                <div className='col-sm-8 col-lg-6 mx-auto text-center '>
+                  <h3 id='softwareDeveloper'>SOFTWARE DEVELOPER</h3>
+                </div>
+              </div>
+
+
+            </div>
+            {/* end container */}
+
+          </div>
+          {/* end section one */}
+
+          <div className="sectionOneBottom d-flex justify-content-center align-items-center ">
+            <a href="#welcome"><i class="fa-solid fa-angles-down arrows"></i></a>
+          </div>
+
         </div>
+        {/* end first slide */}
 
-        <div className='sectionOne d-flex align-items-center'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-7 col-m-9 mx-auto  '>
-                <h1 id='simonOliver'>SIMON OLIVER</h1>
+
+
+
+
+        <div id='welcome' className='sectionTwo d-flex align-items-center '>
+
+          <div className='container-fluid  '>
+            <div className='row offWhite'>
+              <div className=' col-sm-8 col-lg-4 mx-auto text-center '>
+                <h3 className='welcomeAbode'>Welcome to my digital abode.</h3>
+                <img src="/images/digitalAbode.png" id='abodeImage' alt="digital abode" />
+                <p id='abodeText'> While you’re here, check out some of my projects or read about my storied past.</p>
+                <div className='navBar mx-auto text-center'>
+                  <a href="#projects" className='navLinks'>projects</a> <a href="#aboutMe" className='navLinks'>about me</a>
+                </div>
               </div>
             </div>
-            <div className='row pb-5'>
-              <div className='col-6 mx-auto text-end '>
-                <h3 id='softwareDeveloper'>SOFTWARE DEVELOPER</h3>
-              </div>
-            </div>
-
-
           </div>
           {/* end container */}
-
         </div>
-        {/* end section one */}
+        {/* end section two */}
 
-        <div className="sectionOneBottom d-flex justify-content-center align-items-center ">
-          <i class="fa-solid fa-angles-down"></i>
+        <div id='projects' className='sectionThree '>
+
+          <div className='container '>
+            <div className='row'>
+              <h2 id='projectHead'>PROJECTS</h2>
+              <ProjectCarousel />
+            </div>
+          </div>
         </div>
+        {/* end section three */}
 
+        <div id='aboutMe' className='sectionFour d-flex align-items-center'>
 
-        {/* end first slide */}
-      </div>
-
-
-
-
-      <div className='sectionTwo d-flex align-items-center '>
-
-        <div className='container-fluid  '>
-          <div className='row offWhite'>
-            <div className=' col-4 mx-auto text-center '>
-              <h3 className='welcomeAbode'>Welcome to my digital abode.</h3>
-              <img src="/images/digitalAbode.png" id='abodeImage' alt="digital abode" />
-              <p id='abodeText'> While you’re here, check out some of my projects or read about my storied past.</p>
-              <div className='navBar mx-auto text-center'>
-                <a href="http://" className='navLinks'>projects</a> <a href="http://" className='navLinks'>about me</a>
+          <div className='container-fluid'>
+            <div className='row'>
+              <div className="col-5 offset-1">
+                <img src="/images/newhs.jpg" id='simonPhoto' alt="simon photo" />
+              </div>
+              <div className="col-5">
+                <h2 id='niceToMeet'>NICE TO MEET YOU</h2>
+                <div className='scrollable'>
+                  <p className='mb-3'>I’m a full-stack software developer from Minneapolis, Minnesota.
+                    I’ve lived here for most of my life - but I did have a stint in beautiful Des Moines,
+                    Iowa where I obtained my bachelor’s degree from Drake University and afterwards worked
+                    for the state’s Racing and Gaming Commission regulating the online sports
+                    betting industry. After four years in the industry, I made the decision to pursue a
+                    career that would nurture my passion for creative problem solving and also allow
+                    me to acquire a valuable and interesting skill that I could continue to build upon and
+                    explore throughout my entire career.
+                  </p>
+                  <p className='mb-3'>
+                    That’s how I ended up here. I now create full-stack CRUD applications using python,
+                    javascript, C#, React, MySQL, MongoDB, .NET, Entity, and much, much more. I also like to
+                    keep my coding brain fresh by mixing in some game development, using Unity and Godot.
+                  </p>
+                  <p className='mb-3'>
+                    When I’m not coding, I love to stay active, whether that be lifting weights, swimming,
+                    hiking, or climbing the occasional tree. I’m a creative problem solver in the kitchen too -
+                    to the delight or detriment of my friends and family depending on the day - I’ve recently
+                    been obsessed with Mediterranean food. I also enjoy connecting with my out-of-state friends
+                    on discord to chat and game together.
+                  </p>
+                  <p>
+                    Want to know even more about me!?
+                    <a href="mailto:simonallenoliver@gmail.com" className='bioLinks'> Email me </a> or connect with me on
+                    <a href="http://www.linkedin.com/in/simon-oliver-66807213b" className='bioLinks' target="_blank"> Linkedin </a>
+                    <i class="fa-regular fa-face-smile-beam"></i>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        {/* end container */}
-      </div>
-      {/* end section two */}
 
-      <div className='sectionThree '>
-
-        <div className='container '>
-          <div className='row'>
-            <h2 id='projectHead'>PROJECTS</h2>
-            <ProjectCarousel />
-          </div>
-        </div>
-      </div>
-      {/* end section three */}
-
-      <div className='sectionFour d-flex align-items-center'>
-
-        <div className='container-fluid'>
-          <div className='row'>
-            <div className="col-5 offset-1">
-              <img src="/images/newhs.jpg" id='simonPhoto' alt="simon photo" />
-            </div>
-            <div className="col-5">
-              <h2>NICE TO MEET YOU</h2>
-              <div className='scrollable'>
-              <p className='my-3'>I’m a full-stack software developer from Minneapolis, Minnesota.
-                I’ve lived here for most of my life - but I did have a stint in beautiful Des Moines,
-                Iowa where I obtained my bachelor’s degree from Drake University, and afterwards worked
-                for the State’s Racing and Gaming Commission regulating the exciting, online sports
-                betting industry. After four years in the industry, I made the decision to pursue a
-                career that would nurture my passion for creative problem solving; one that would allow
-                me to acquire a valuable and interesting skill that I could continue to build upon and
-                explore throughout my entire career.
-              </p>
-              <p className='mb-3'>
-                That’s how I ended up here. I now create full-stack CRUD applications using python,
-                javascript, C#, React, MySQL, MongoDB, .NET, Entity, and much, much more. I also like to
-                keep my coding brain fresh by mixing in some game development, using Unity and Godot
-              </p>
-              <p className='mb-3'>
-                When I’m not coding, I love to stay active, whether that be lifting weights, swimming,
-                hiking, or climbing the occasional tree. I’m a creative problem solver in the kitchen too -
-                to the delight or detriment of my friends and family depending on the day - I’ve recently
-                been obsessed with Mediterranean food. I also enjoy connecting with my out of state friends
-                on discord to chat and game together.
-              </p>
-              <p>
-                Want to know even more about me!? Email me or connect with me on Linkedin :)
-              </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        {/* end giant container */}
       </div>
     </>
   )
--- client/src/components/ProjectCarousel.jsx
+++ client/src/components/ProjectCarousel.jsx
@@ -3,91 +3,100 @@ import React from 'react'
 const ProjectCarousel = () => {
     return (
         <>
-            <div className="carousel slide" id='carouselActions' data-bs-wrap="true">
+            <div className="carousel slide" id='carouselActions' 
+            data-bs-wrap="true">
                 <div className="carousel-inner">
-                    <div className="carousel-item active">
+
+                    <div className="carousel-item active"
+                    data-bs-interval="13000">
                         <div className="row">
-                            <div className='col-2'>
+                            <div className='col-1'>
                                 <button className="carousel-control-prev"
                                     type="button"
                                     data-bs-target="#carouselActions"
                                     data-bs-slide="prev">
-                                    prev
+                                    <i class="fa-solid fa-chevron-left"></i>
                                 </button>
                             </div>
-
-                            <div className='col-4 mx-auto text-center'><img src="/images/leafySC.png" alt="img1" /></div>
+                            <div className='col-5 mx-auto text-center projectImage'><img src="/images/leafySC.png" alt="leafy website screenshot" /></div>
                             <div className='col-4 mx-auto '>
-                                <h3>project title</h3>
-                                <p>Project text description wow such cool project I love this fuin wow
-                                    thanks yes fair cool very exciting hope it is good project</p>
-                                <button>view project!</button>
+                                <h3>Leafy</h3>
+                                <p className='py-3'>A plant ecommerce website I built with MongoDB, Express, React, and Node. 
+                                Features: Modular design allows for quick reordering of site, plus easily recyclable components for future projects;
+                                React enables super efficient rendering through use of its virtual DOM, as well as cleaner, direct code using JSX embedded in HTML. </p>
+                                <button className='viewButton'>view project!</button>
                             </div>
-                            <div className='col-2 text'>
+                            <div className='col-1 '>
                                 <button className="carousel-control-next"
                                     type="button"
                                     data-bs-target="#carouselActions"
                                     data-bs-slide="next">
-                                    next
+                                    <i class="fa-solid fa-chevron-right"></i>
                                 </button>
                             </div>
                         </div>
 
                     </div>
 
-                    <div className="carousel-item">
+                    <div className="carousel-item" data-bs-interval="12000">
                         <div className="row">
-                            <div className='col-2'>
+                            <div className='col-1'>
                                 <button className="carousel-control-prev"
                                     type="button"
                                     data-bs-target="#carouselActions"
                                     data-bs-slide="prev">
-                                    prev
+                                    <i class="fa-solid fa-chevron-left"></i>
                                 </button>
                             </div>
 
-                            <div className='col-4 mx-auto text-center'><img src="/images/leafySC.png" alt="img1" /></div>
+                            <div className='col-5 mx-auto text-center projectImage'><img src="/images/sandScreenShot.png" alt="sand game screenshot" /></div>
                             <div className='col-4 mx-auto '>
-                                <h3>project wow</h3>
-                                <p>Project text defgbdfgdfghscription wow such cool what is ur goe this fuin wow
-                                    thanks yes fair ok ok very hhhexciting hope it is good project</p>
-                                <button>view project!</button>
+                                <h3>Sand</h3>
+                                <p className='py-3'>A short browser interactive story I built using Python, Flask, and MySQL. Features: 
+                                Queries facilitate trade betweeen different character inventories - each item in an inventory represents a row in the database;
+                                Validations produce flash messsages that alert the player when they attempt to make an invalid trade.
+                                </p>
+
+                                <button className='viewButton'>view project!</button>
                             </div>
-                            <div className='col-2 text'>
+                            <div className='col-1 '>
                             <button className="carousel-control-next"
                                     type="button"
                                     data-bs-target="#carouselActions"
                                     data-bs-slide="next">
-                                    next
+                                    <i class="fa-solid fa-chevron-right"></i>
                                 </button>
                             </div>
                         </div>
 
                     </div>
 
-                    <div className="carousel-item">
+                    <div className="carousel-item" data-bs-interval="12000">
                         <div className="row">
-                            <div className='col-2'>
+                            <div className='col-1'>
                                 <button className="carousel-control-prev"
                                     type="button"
                                     data-bs-target="#carouselActions"
                                     data-bs-slide="prev">
-                                    prev
+                                    <i class="fa-solid fa-chevron-left"></i>
                                 </button>
                             </div>
 
-                            <div className='col-4 mx-auto text-center'><img src="/images/leafySC.png" alt="img1" /></div>
+                            <div className='col-5 mx-auto text-center projectImage'><img src="/images/forsakenSC.png" alt="forsaken forest game screenshot" /></div>
                             <div className='col-4 mx-auto '>
-                                <h3>project three</h3>
-                                <p>Proanotha one omg yes such impress full of rpojecet hope it is good project</p>
-                                <button>view project!</button>
+                                <h3>Forsaken Forest</h3>
+                                <p className='py-3'>A top-down RPG I made using the Godot game engine's Python-like language, GDScript. 
+                                Features: Over ten complex, conditional character animations;
+                                Enemies programmed to pursue the player character once they have reached a certain proximity;
+                                Combat functions that result in removing character sprites from the game</p>
+                                <button className='viewButton'>view project!</button>
                             </div>
-                            <div className='col-2 text'>
+                            <div className='col-1 '>
                             <button className="carousel-control-next"
                                     type="button"
                                     data-bs-target="#carouselActions"
                                     data-bs-slide="next">
-                                    next
+                                    <i class="fa-solid fa-chevron-right"></i>
                                 </button>
                             </div>
                         </div>
